cda4e01a93374d1f97c5f1519451ebdb2ac3e0ce
einar-bjarnason <einar.bjarnason@evry.com>
2019-10-01 10:43:11 +0200

    hardcoded, but works

--- install_ansible.sh
+++ install_ansible.sh
@@ -7,6 +7,8 @@ green=`tput setaf 2`
 reset=`tput sgr0`
 # echo "${red}red text ${green}green text${reset}"
 SPC="software-properties-common"
+LOC="/etc/ansible"
+FOLDERS="facts files inventory playbooks plugins roles inventory/group_vars inventory/host_vars"
 
 # use != and remove 'NOT' if it must be run as root
 if [ "$(whoami)" != 'root' ]; then
@@ -14,18 +16,38 @@ if [ "$(whoami)" != 'root' ]; then
   exit 1
 fi
 
-printf "\n~~ Ansible Install Script ~~\n\nInstalling prerequisites\n"
+printf "\n~~ Ansible Install Script ~~\n"
+sleep 1
+printf "\nPreparing system\n"
 
-#apt-get update
+printf "\nUpdating apt cache\n"
+apt-get update > /dev/null 2>&1
 
-if ! dpkg -s $SPC >/dev/null 2>&1;
-then
- printf "package '$SPC' is installed ...continuing\n"
-else
-  printf "package $SPC will be installed"
-  apt-get install -y $SPC
-fi
+printf "\nInstalling required packages\n"
+apt-get install -y $SPC python-pip python-dev libkrb5-dev gcc > /dev/null 2>&1
 
-# grep -r "ansible" /etc/apt/sources.list*
+printf "\nRemoving any old Ansible PPAs\n"
+add-apt-repository -ry ppa:ansible/ansible > /dev/null 2>&1
 
-# apt-add-repository -y ppa:ansible/ansible
+printf "\nAdding Ansible PPA\n"
+add-apt-repository -y ppa:ansible/ansible > /dev/null 2>&1
+
+printf "\nUpdating apt cache\n"
+apt-get update > /dev/null 2>&1
+
+printf "\nInstalling Ansible\n"
+apt-get install -y ansible > /dev/null 2>&1
+
+printf "\nInstalling python modules\n"
+pip install pykerberos pywinrm py > /dev/null 2>&1
+
+printf "\nSetting Ansible permissions\n"
+chown root:ansible $LOC
+chmod g+s $LOC
+
+printf "\nCreate any missing directories\n"
+for dir in $FOLDERS;
+do
+ mkdir -p $LOC/$dir
+done
+
